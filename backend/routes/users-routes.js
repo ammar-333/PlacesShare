75957b1a2fad5a9407c8e889e77b61e5578103aa
ammar <ammarshaban5@gmail.com>
2025-11-30 12:17:07 +0300

    finish

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const { check } = require('express-validator');
+
+const usersController = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
+
 const router = express.Router();
 
-const usersController = require("../controllers/users-controller");
+router.get('/', usersController.getUsers);
 
-// api/users
-router.get('/', usersController.getAllUsers);
+router.post(
+  '/signup',
+  fileUpload.single('image'),
+  [
+    check('name')
+      .not()
+      .isEmpty(),
+    check('email')
+      .normalizeEmail()
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  usersController.signup
+);
 
-// api/user/signup
-router.post('/signup', usersController.signup);
-
-// api/user/login
 router.post('/login', usersController.login);
-
-
 
 module.exports = router;
